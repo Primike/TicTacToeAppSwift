111672230c7012a65e318687a8b801a0500a7b87
Prince Avecillas <avecillasprince@yahoo.com>
2022-03-10 20:45:50 -0500

    App done

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -62,9 +62,11 @@ class ViewController: UIViewController {
         }
         else {
             if color == UIColor.blue {
+                winLabel.text = "Reds Turn!"
                 player1.append(x)
             }
             else {
+                winLabel.text = "Blues Turn!"
                 player2.append(x)
             }
             actionz(sender: sender)
@@ -118,7 +120,44 @@ class ViewController: UIViewController {
     }
     
     @IBOutlet weak var winLabel: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
     
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        button1.backgroundColor = UIColor.systemGray
+        button2.backgroundColor = UIColor.systemGray
+        button3.backgroundColor = UIColor.systemGray
+        button4.backgroundColor = UIColor.systemGray
+        button5.backgroundColor = UIColor.systemGray
+        button6.backgroundColor = UIColor.systemGray
+        button7.backgroundColor = UIColor.systemGray
+        button8.backgroundColor = UIColor.systemGray
+        button9.backgroundColor = UIColor.systemGray
+        
+        button1.isEnabled = true
+        button2.isEnabled = true
+        button3.isEnabled = true
+        button4.isEnabled = true
+        button5.isEnabled = true
+        button6.isEnabled = true
+        button7.isEnabled = true
+        button8.isEnabled = true
+        button9.isEnabled = true
+        
+        color = UIColor.blue
+        player1 = [Int]()
+        player2 = [Int]()
+        completed = false
+        winLabel.text = "Click to Play!"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
